12c2c516f942bffdefe84e371950f343ab12b1d9
Taylor James Bell <taylor.bell@mail.mcgill.ca>
2018-12-20 13:30:08 +0000

    Updating version number to 1.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,7 @@ author = 'Taylor James Bell'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '1.1'
+release = '1.2'
 
 
 # -- General configuration ---------------------------------------------------
